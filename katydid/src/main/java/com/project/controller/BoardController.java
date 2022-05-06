b5a8196630b271a870ba6062db5c30a0b5b542ff
jjjjjjji-hun <lingcke@naver.com>
2022-05-06 16:29:35 +0900

    김지훈 : 학원에서 작업

--- katydid/src/main/java/com/project/controller/BoardController.java
+++ katydid/src/main/java/com/project/controller/BoardController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.project.domain.BoardVO;
 import com.project.domain.PageMaker;
 import com.project.domain.SearchCriteria;
+import com.project.domain.StoreVO;
 import com.project.service.board.BoardService;
 
 import lombok.extern.log4j.Log4j;
@@ -61,9 +62,9 @@ public class BoardController {
 	
 	@PostMapping("/insert")
 	public String boardInsert(BoardVO vo, Model model) {
-			
+		StoreVO stvo = new StoreVO();
 		log.info("들어온 데이터 디버깅 : " + vo);
-		service.insert(vo);
+		service.insert(vo, stvo);
 			
 		return "redirect:list/";
 	}
