a8f9b95a2254a50ba424289b77a3a8a3e0763fbc
eyonghwan <popularisyong@gmail.com>
2022-05-15 18:58:22 +0900

    이용환 : 채팅기능 추가

--- katydid/src/main/java/com/project/controller/HomeController.java
+++ katydid/src/main/java/com/project/controller/HomeController.java
@@ -6,10 +6,14 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,6 +40,11 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "main";
+	}
+	
+	@GetMapping("/chatting")
+	public String chat(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+		return "chat";
 	}
 	
 }
